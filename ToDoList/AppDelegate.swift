fb38e343f93b44ec5b6d4cf78c8f0a4d6843ad98
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-18 03:42:29 -0400

    updated AppDelegate.swift to get the default Realm

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-
+  
+  // Get the default Realm
+  let todoRealmDB = try! Realm()
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
