c7ace48863190d0bf4e69250790ff214d73defb1
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-18 02:54:25 -0400

    implemented delete functionality

--- ToDoList/TasksTableViewController.swift
+++ ToDoList/TasksTableViewController.swift
@@ -13,109 +13,112 @@ class TasksTableViewController: UITableViewController {
   
   
   
-    //MARK: Properties
+  //MARK: Properties
   
-    var tasks = [Task]()
+  var tasks = [Task]()
   
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      // Load the sample data.
-      loadSampleToDoItems()
-      
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
-    }
-
-    let cellIdentifier = "TaskTableViewCell"
-    let dateFormatter = DateFormatter()
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-          as? TaskTableViewCell  else {
-            fatalError("The dequeued cell is not an instance of TableTableViewCell.")
-      }
-      let task = tasks[indexPath.row]
-      cell.nameLabel.text = task.name
-      cell.daydatetimeLabel.text = dateFormatter.string(from: task.date as Date)
-      if(task.priority == 1){
-        cell.priorityLabel.text = ""
-      }
-      if(task.priority == 2){
-        cell.priorityLabel.text = "!"
-      }
-      if(task.priority == 3){
-        cell.priorityLabel.text = "!!"
-      }
-      if(task.priority == 4){
-        cell.priorityLabel.text = "!!!"
-      }
-      return cell
-    }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
   
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      super.prepare(for: segue, sender: sender)
-      
-      switch(segue.identifier ?? "") {
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-      case "AddItem":
+    navigationItem.leftBarButtonItem = editButtonItem
+    
+    // Load the sample data.
+    loadSampleToDoItems()
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count
+  }
+  
+  let cellIdentifier = "TaskTableViewCell"
+  let dateFormatter = DateFormatter()
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+      as? TaskTableViewCell  else {
+        fatalError("The dequeued cell is not an instance of TableTableViewCell.")
+    }
+    let task = tasks[indexPath.row]
+    cell.nameLabel.text = task.name
+    cell.daydatetimeLabel.text = dateFormatter.string(from: task.date as Date)
+    if(task.priority == 1){
+      cell.priorityLabel.text = ""
+    }
+    if(task.priority == 2){
+      cell.priorityLabel.text = "!"
+    }
+    if(task.priority == 3){
+      cell.priorityLabel.text = "!!"
+    }
+    if(task.priority == 4){
+      cell.priorityLabel.text = "!!!"
+    }
+    return cell
+  }
+  
+  
+  
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+ 
+  
+  
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+  tasks.remove(at: indexPath.row)
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+  
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    super.prepare(for: segue, sender: sender)
+    
+    switch(segue.identifier ?? "") {
+      
+    case "AddItem":
       os_log("Adding a new task.", log: OSLog.default, type: .debug)
       
       
-      case "ShowDetail":
+    case "ShowDetail":
       guard let mealDetailViewController = segue.destination as? TaskViewController else {
         fatalError("Unexpected destination: \(segue.destination)")
       }
@@ -130,10 +133,10 @@ class TasksTableViewController: UITableViewController {
       
       let selectedTask = tasks[indexPath.row]
       mealDetailViewController.task = selectedTask
-        
-      default:
+      
+    default:
       fatalError("Unexpected Segue Identifier; \(segue.identifier)")
-      }
+    }
   }
   
   //MARK: Actions
@@ -149,16 +152,16 @@ class TasksTableViewController: UITableViewController {
         
       }
       else{
-      // Add a new to-do task.
-      let newIndexPath = IndexPath(row: tasks.count, section: 0)
-      
-      tasks.append(task!)
-      tableView.insertRows(at: [newIndexPath], with: .automatic)
+        // Add a new to-do task.
+        let newIndexPath = IndexPath(row: tasks.count, section: 0)
+        
+        tasks.append(task!)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
       }
     }
   }
   
-
+  
   
   //MARK: Private Methods
   
