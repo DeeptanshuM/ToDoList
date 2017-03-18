414e93dc613dcc98297169afe0e4d40cffe53b8f
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-18 04:22:29 -0400

    can now delete data drom realm as well

--- ToDoList/TasksTableViewController.swift
+++ ToDoList/TasksTableViewController.swift
@@ -71,24 +71,27 @@ class TasksTableViewController: UITableViewController {
   
   
   
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
- 
+  // Override to support conditional editing of the table view.
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
   
   
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-  tasks.remove(at: indexPath.row)
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
+  
+  // Override to support editing the table view.
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      // Delete the row from the data source
+      realm.beginWrite()
+      realm.delete(tasks[indexPath.row])
+      try! realm.commitWrite()
+      tasks.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+  }
   
   
   /*
